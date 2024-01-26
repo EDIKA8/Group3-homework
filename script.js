d40b679fbf977865e449826393b7fe7a029380fe
Soso Valishvili <131302809+EDIKA8@users.noreply.github.com>
2024-01-26 21:25:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let numString = "46";
+
+let num = Number(numString);
+console.log(num); 
+
+let str = String(num);
+console.log(str);  
+
+
+let x = true;
+let y = false;
+
+console.log(x && y);  // Output: false
+console.log(x && true);  // Output: true
